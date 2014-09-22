ffd52048266e5403ab62dad87f5cb49e1fbdcb57
Anna Petry <hello@annapetry.com>
2014-09-22 15:20:48 -0700

    Added list of users to index

--- public/javascripts/chat_ui.js
+++ public/javascripts/chat_ui.js
@@ -6,7 +6,17 @@
   var ChatUi = window.App.ChatUi = function (options) {
     this.chat = options.chat;
     this.$rootEl = options.$rootEl;
-  }
+  };
+  
+  ChatUi.prototype.displayUsers = function (usersHash) {
+    this.$rootEl.find('#users').empty();
+    var that = this;
+    Object.keys(usersHash).forEach(function (key) {
+      var $li = $('<li></li>');
+      $li.text(usersHash[key]);
+      that.$rootEl.find('#users').prepend($li);
+    });
+  };
 
   ChatUi.prototype.getMessage = function () {
     var message = this.$rootEl.find('#msg').val();
@@ -33,7 +43,12 @@ $(function () {
    var ui = new App.ChatUi({
      chat: chat,
      $rootEl: $('body')
-   })
+   });
+   
+  socket.on('updateUsers', function (data) {
+    ui.displayUsers(data.users);
+  });
+   
   socket.on('message', function (data) {
     var message = '< ' + data.nick + ' > ' + data.text;
     ui.addMessage(message);
